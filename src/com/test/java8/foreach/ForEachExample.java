72113218bc87a53a6992ecc32b1ace92ade49fd3
Siddayya <siddusthawarmath@gmail.com>
2018-04-30 12:03:13 +0530

    for example

---
+++ src/com/test/java8/foreach/ForEachExample.java
@@ -0,0 +1,21 @@
+
+package com.test.java8.foreach;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachExample {
+
+	public static void main(String[] args) {
+
+		List<String> names = new ArrayList<>();
+		names.add("Suresh");
+		names.add("Ganesh");
+		names.add("Raghav");
+		names.add("Raj");
+
+		//Display all names
+		names.forEach(name -> System.out.println(name));
+
+	}
+}
