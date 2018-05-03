205bea2126fdfd8d42fa87c98f4abca31021e7b9
Siddayya <siddusthawarmath@gmail.com>
2018-05-03 15:47:51 +0530

    deleted bin

--- src/com/test/stream/SortFindFirstExample.java
+++ src/com/test/stream/SortFindFirstExample.java
@@ -3,13 +3,17 @@ package com.test.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-
+//Sort integer numbers and find out first number
 public class SortFindFirstExample {
 
 	public static void main(String[] args) {
 
+		//Sort integer numbers and find out first number
 		List intNumbers = Arrays.asList(100, 30, 8, 18);
+		
 		Optional firstNum = intNumbers.stream().sorted().findFirst();
+
+		
 		System.out.println(firstNum.get());
 
 	}
