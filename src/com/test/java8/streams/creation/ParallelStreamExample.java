032dfa6752f098a22265bc645d7763885f2579d7
Siddu <siddusthawarmath@gmail.com>
2018-10-17 17:42:50 +0530

    java 8 example

--- src/com/test/java8/streams/creation/ParallelStreamExample.java
+++ src/com/test/java8/streams/creation/ParallelStreamExample.java
@@ -1,8 +1,9 @@
 package com.test.java8.streams.creation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -19,16 +20,5 @@ public class ParallelStreamExample {
 		// Prints all the elements but order is no guaranty
 		System.out.println("----Parallel Stream--");
 		names.stream().parallel().forEach(name -> System.out.println(name));
-
-		System.out.println("------To check performance-----------");
-
-		// =new ArrayList<>();//Arrays.asList(10,3,5,6,8,9,7);
-
-		int[] numbers = IntStream.range(1, 100).map((x) -> {
-			return (int) Math.random() * 100;
-		}).toArray();
-
-	Stream.of(numbers).mapToInt(x-> x). filter().collect(Collectors.toList());	
-		
 	}
 }
